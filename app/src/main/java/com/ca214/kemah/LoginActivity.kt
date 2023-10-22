721461e3fc4c2a8bb7fb83465f56d818586bcc56
I Wayan Jepriana <jepriana.id@gmail.com>
2023-10-22 14:41:40 +0800

    Basic Intent & With Data

--- app/src/main/java/com/ca214/kemah/LoginActivity.kt
+++ app/src/main/java/com/ca214/kemah/LoginActivity.kt
@@ -1,11 +1,57 @@
 package com.ca214.kemah
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+    // Deklarasi object yang terhubung dengan layout
+    private lateinit var btnLogin: Button
+    private lateinit var btnRegister: Button
+    private lateinit var editEmail: EditText
+    private lateinit var editPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Inisialisasi object
+        btnLogin = findViewById(R.id.button_login)
+        btnRegister = findViewById(R.id.button_register)
+        editEmail = findViewById(R.id.edit_email)
+        editPassword = findViewById(R.id.edit_password)
+
+        btnLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.button_login -> {
+                // Mengambil data dari text entry
+                val inputEmail = editEmail.text.toString().trim()
+                val inputPassword = editPassword.text.toString().trim()
+                var invalidEntries = false
+
+                // Validasi input
+                if (inputEmail.isEmpty()) {
+                    invalidEntries = true
+                    editEmail.setError("Email is empty")
+                }
+                if (inputPassword.isEmpty()) {
+                    invalidEntries = true
+                    editPassword.setError("Password is empty")
+                }
+
+                if (!invalidEntries) {
+                    // Membangung object intent untuk membuka main activity
+                    val openMainActivity = Intent(this@LoginActivity, MainActivity::class.java)
+                    // Menjalankan activity menggunakan intent
+                    startActivity(openMainActivity)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/ca214/kemah/MainActivity.kt
+++ app/src/main/java/com/ca214/kemah/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.ca214.kemah
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var fabAddCampground: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fabAddCampground = findViewById(R.id.fab_add_campground)
+
+        fabAddCampground.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.fab_add_campground -> {
+                val openCampgroundEntry = Intent(this@MainActivity, CampgroundEntryActivity::class.java)
+                startActivity(openCampgroundEntry)
+            }
+        }
     }
 }
